5d7d26b7845b87f3ad96e4fecedadd980a054607
mingliu <ming-liu@outlook.com>
2023-01-14 23:41:42 -0600

    242

---
+++ 242.valid-anagram.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=242 lang=python3
+#
+# [242] Valid Anagram
+#
+
+# @lc code=start
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        d = {}
+        for i in s:
+            if i in d:
+                d[i] += 1
+            else:
+                d[i] = 1
+        for i in t:
+            if i in d:
+                d[i] -= 1
+            else:
+                return False
+        for i in d:
+            if d[i] != 0:
+                return False
+            else:
+                return True
+
+# @lc code=end
