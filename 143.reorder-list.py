97617e8f259bc331b911629b68dea54190835afb
mingliu <ming-liu@outlook.com>
2023-02-03 10:48:22 -0600

    143

---
+++ 143.reorder-list.py
@@ -0,0 +1,103 @@
+#
+# @lc app=leetcode id=143 lang=python3
+#
+# [143] Reorder List
+#
+# https://leetcode.com/problems/reorder-list/description/
+#
+# algorithms
+# Medium (49.94%)
+# Likes:    8073
+# Dislikes: 277
+# Total Accepted:    627.8K
+# Total Submissions: 1.2M
+# Testcase Example:  '[1,2,3,4]'
+#
+# You are given the head of a singly linked-list. The list can be represented
+# as:
+#
+#
+# L0 → L1 → … → Ln - 1 → Ln
+#
+#
+# Reorder the list to be on the following form:
+#
+#
+# L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+#
+#
+# You may not modify the values in the list's nodes. Only nodes themselves may
+# be changed.
+#
+#
+# Example 1:
+#
+#
+# Input: head = [1,2,3,4]
+# Output: [1,4,2,3]
+#
+#
+# Example 2:
+#
+#
+# Input: head = [1,2,3,4,5]
+# Output: [1,5,2,4,3]
+#
+#
+#
+# Constraints:
+#
+#
+# The number of nodes in the list is in the range [1, 5 * 10^4].
+# 1 <= Node.val <= 1000
+#
+#
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        # use slow fast pointer to find the middle of the linked list
+        # revser order of the 2nd half linked list
+        # iterate through both lists and merge
+
+        # find the middle point
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second = slow.next
+        slow.next = None
+
+        # reverse the 2nd portion of the list
+        prev = None
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
+
+        # 2nd list now head is prev
+        # 1st list head is head
+
+        # merge 2 halfs
+        first, second = head, prev
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first = temp1
+            second = temp2
+        return head
+
+
+# @lc code=end
