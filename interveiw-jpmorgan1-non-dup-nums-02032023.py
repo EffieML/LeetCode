d967dc3a9ec86d99f2a246cf237baac01fa6217a
mingliu <ming-liu@outlook.com>
2023-02-06 14:52:11 -0600

    jpmorgan

--- interveiw-jpmorgan1-non-dup-nums-02032023.py
+++ interveiw-jpmorgan1-non-dup-nums-02032023.py
@@ -1,3 +1,30 @@
 # input [2,2,[1,19],[9,19]]
 # where [1, 9] means[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
 # output only print no return as 18, 10
+
+def nonDupNums(arr):
+    result = []
+    duplicate_set = set()
+    for elem in arr:
+        if type(elem) == int:
+            # str_elem = str(elem)
+            # if len(str_elem) == len(set(str_elem)):
+            result.append(0)
+            # else:
+            #     result.append(1)
+        elif type(elem) == list:
+            count = 0
+            for i in range(elem[0], elem[1]+1):
+                str_i = str(i)
+                # if str_i in duplicate_set:
+                #     continue
+                if len(str_i) == len(set(str_i)):
+                    count += 1
+                # else:
+                #     duplicate_set.add(str_i)
+            result.append(count)
+    return result
+
+
+arr = [2, 2, [1, 19], [9, 19], [110, 119]]
+print(nonDupNums(arr))
--- interview-jpmorgan1-string-permutations-next-02032023.py
+++ interview-jpmorgan1-string-permutations-next-02032023.py
@@ -1,2 +1,26 @@
 # give you a string 'bac', return 'bca' which is the permutation of the string, the next of the input.
 # bac permutation array [abc, acb, bac, bca, cab, cba]
+
+def nextPermutation(s):
+    s = list(s)
+    # ['b','a','c']
+    print(s)
+    i = len(s) - 2
+    while i >= 0 and s[i] >= s[i + 1]:
+        i -= 1
+    if i == -1:
+        s.reverse()
+        return ''.join(s)
+        'cab'
+
+    print(s)
+    j = len(s) - 1
+    while s[j] <= s[i]:
+        j -= 1
+    s[i], s[j] = s[j], s[i]
+    s[i + 1:] = reversed(s[i + 1:])
+    return ''.join(s)
+
+
+s = 'bac'
+nextPermutation(s)
