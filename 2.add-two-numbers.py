d32a082a8f65c36d7c9b01a6949ca274e619fa81
mingliu <ming-liu@outlook.com>
2023-02-06 11:02:02 -0600

    2

---
+++ 2.add-two-numbers.py
@@ -0,0 +1,94 @@
+#
+# @lc app=leetcode id=2 lang=python3
+#
+# [2] Add Two Numbers
+#
+# https://leetcode.com/problems/add-two-numbers/description/
+#
+# algorithms
+# Medium (39.32%)
+# Likes:    24203
+# Dislikes: 4676
+# Total Accepted:    3.4M
+# Total Submissions: 8.5M
+# Testcase Example:  '[2,4,3]\n[5,6,4]'
+#
+# You are given two non-empty linked lists representing two non-negative
+# integers. The digits are stored in reverse order, and each of their nodes
+# contains a single digit. Add the two numbers and return the sum as a linked
+# list.
+#
+# You may assume the two numbers do not contain any leading zero, except the
+# number 0 itself.
+#
+#
+# Example 1:
+#
+#
+# Input: l1 = [2,4,3], l2 = [5,6,4]
+# Output: [7,0,8]
+# Explanation: 342 + 465 = 807.
+#
+#
+# Example 2:
+#
+#
+# Input: l1 = [0], l2 = [0]
+# Output: [0]
+#
+#
+# Example 3:
+#
+#
+# Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+# Output: [8,9,9,9,0,0,0,1]
+#
+#
+#
+# Constraints:
+#
+#
+# The number of nodes in each linked list is in the range [1, 100].
+# 0 <= Node.val <= 9
+# It is guaranteed that the list represents a number that does not have leading
+# zeros.
+#
+#
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# iterate once, just add the 2 list, if sum is bigger than 10, store the 10th as carry for the next iteration
+# 1. create dummy node, set as curr
+# 2. set carry =0
+# 3. if l1.val or l2.val or carry exist, then we can add them
+# 4. use result to calculate new carry and new val, and created new List Node
+# 5. move to next location
+# T= O(n), M=O(n)
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        curr = dummy
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            # compute new digit
+            val = v1+v2+carry
+            # calculate new carry and new val
+            carry = val // 10
+            val = val % 10
+            curr.next = ListNode(val)
+            # update pointers for l1, l2 and sum list to the next
+            curr = curr.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummy.next
+
+# @lc code=end
