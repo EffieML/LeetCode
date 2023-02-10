5c97d6783ac4e81d88abc91b6c8883bb2df6f319
mingliu <ming-liu@outlook.com>
2023-02-10 14:40:26 -0600

    20

---
+++ 20.valid-parentheses.py
@@ -0,0 +1,75 @@
+#
+# @lc app=leetcode id=20 lang=python3
+#
+# [20] Valid Parentheses
+#
+# https://leetcode.com/problems/valid-parentheses/description/
+#
+# algorithms
+# Easy (40.80%)
+# Likes:    17963
+# Dislikes: 1015
+# Total Accepted:    3M
+# Total Submissions: 7.5M
+# Testcase Example:  '"()"'
+#
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and
+# ']', determine if the input string is valid.
+#
+# An input string is valid if:
+#
+#
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Every close bracket has a corresponding open bracket of the same type.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: s = "()"
+# Output: true
+#
+#
+# Example 2:
+#
+#
+# Input: s = "()[]{}"
+# Output: true
+#
+#
+# Example 3:
+#
+#
+# Input: s = "(]"
+# Output: false
+#
+#
+#
+# Constraints:
+#
+#
+# 1 <= s.length <= 10^4
+# s consists of parentheses only '()[]{}'.
+#
+#
+#
+
+# @lc code=start
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = {")": "(", "]": "[", "}": "{"}
+
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return True if not stack else False
+
+# @lc code=end
