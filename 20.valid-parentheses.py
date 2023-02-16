2a1302f66ea47f00ff5162e898810e31c86ac1ed
mingliu <ming-liu@outlook.com>
2023-02-16 17:19:35 -0600

    20

--- 20.valid-parentheses.py
+++ 20.valid-parentheses.py
@@ -72,4 +72,18 @@ class Solution:
                 stack.append(c)
         return True if not stack else False
 
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         stack = []
+
+#         for c in s:
+#             if c == "{" or c == "[" or c == "(":
+#                 stack.append(c)
+#             else:
+#                 if c == "}" and stack[-1] != "{" or c == ")" and stack[-1] != "(" or c == "]" and stack[-1] != "[":
+#                     return False
+#                 else:
+#                     stack.pop()
+#             return not stack
+
 # @lc code=end
