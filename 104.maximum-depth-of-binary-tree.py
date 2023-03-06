bc43a3b84c3e6b9ddd8a9e235722cdd1707266d4
mingliu <ming-liu@outlook.com>
2023-03-06 09:59:30 -0600

    104

--- 104.maximum-depth-of-binary-tree.py
+++ 104.maximum-depth-of-binary-tree.py
@@ -50,7 +50,42 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
+
+
 class Solution:
+
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # recursion
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+        # BFS breadth first search
+        # if not root:
+        #     return 0
+        # level = 0
+        # q= deque([root])
+        # while q:
+        #     for i in range(len(q)):
+        #         node = q.popleft()
+        #         if node.left:
+        #             q.append(node.left)
+        #         if node.right:
+        #             q.append(node.right)
+        #     level +=1
+        # return level
+
+        # DFS iterative depth first search
+        # depth first search without recursion
+        # stack =[[root, 1]]
+        # res = 0
+        # while stack:
+        #     node, depth = stack.pop()
+        #     if node:
+        #         res = max(res, depth)
+        #         stack.append([node.left, depth+1])
+        #         stack.append([node.right, depth+1])
+        # return res
 
         # @lc code=end
