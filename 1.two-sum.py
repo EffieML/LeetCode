166f2308fa13d91e748d3fc0d983a09100c3f6ed
mingliu <ming-liu@outlook.com>
2023-03-14 14:47:58 -0500

    1

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -7,12 +7,18 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = {}
-        for i in range(len(nums)):
-            if target - nums[i] in d:
-                return [i, d[target-nums[i]]]
-            else:
-                d[nums[i]] = i
+        # d = {}
+        # for i in range(len(nums)):
+        #     if target - nums[i] in d:
+        #         return [i, d[target-nums[i]]]
+        #     else:
+        #         d[nums[i]] = i
 
+        d = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in d:
+                return [d[diff], i]
+            d[n] = i
 # time O(n) worst time O(n^2), space O(n)
 # @lc code=end
